afdf95e9aa9cd951ef6f850af74d944400a930ea
HamzaYslmn <hamzayesilmen7@gmail.com>
2024-07-13 21:57:51 +0300

    5ms Simple

--- CODE/V3/esp32wifi/esp32wifi.ino
+++ CODE/V3/esp32wifi/esp32wifi.ino
@@ -11,10 +11,14 @@ AsyncUDP udp;
 void setupWiFi() {
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    vTaskDelay(10 / portTICK_PERIOD_MS);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed");
+    while (1) {
+      delay(1000);
+    }
   }
-  Serial.printf("Connected to WiFi. IP: %s\n", WiFi.localIP().toString().c_str());
+  Serial.print("Connected to WiFi. IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void handleUDPPacket(AsyncUDPPacket packet) {
@@ -22,13 +26,14 @@ void handleUDPPacket(AsyncUDPPacket packet) {
   udp.writeTo(packet.data(), packet.length(), packet.remoteIP(), packet.remotePort());
   uint32_t sendTime = micros();
   
-  float processingTime = (sendTime - receiveTime) / 1000.0; // Convert to ms
-  Serial.printf("Echo: %.*s, Processing time: %.3f ms\n", packet.length(), packet.data(), processingTime);
+  float processingTime = (sendTime - receiveTime) / 1000.0;
+  String message = String((char*)packet.data());
+  Serial.println("Echo: " + message + " Latency: " + String(processingTime) + " ms");
 }
 
 void setupUDP() {
   if (udp.listen(UDP_PORT)) {
-    Serial.printf("UDP listening on port %d\n", UDP_PORT);
+    Serial.println("UDP listening on port " + String(UDP_PORT));
     udp.onPacket(handleUDPPacket);
   }
 }
